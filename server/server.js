cf55da03ed4e20e1b4e3e2a5ffcde739cdfc0bf4
Bryan Bickel <briimcfly@gmail.com>
2023-11-06 10:24:57 -0800

    adjusted server for Apollo App

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
+
+//Auth Util
+const {authMiddleware} = require('./utils/auth');
+
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
@@ -9,13 +15,27 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+async function startApolloServer(typeDefs,resolvers){
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`GraphQL Running @ http://localhost:${PORT}${server.graphqlPath}`);
+}
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//Old REST API Routes
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer(typeDefs,resolvers);
