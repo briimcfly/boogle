0eb8d29471b8cc0f17504a98104c1d2edd8af4f5
Bryan Bickel <briimcfly@gmail.com>
2023-11-06 11:43:09 -0800

    added saveBook and removeBook resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -26,6 +26,35 @@ const resolvers = {
 
             const token = signToken(user);
             return{token, user};
+        },
+        addUser: async (_,{username, email, password}) => {
+            const user = await User.create({username, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+        saveBook: async(_, {bookInput}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    context.user._id,
+                    {$addToSet: {savedBooks: bookInput}},
+                    {new: true, runValdators: true}
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async(_, {bookId}, context) => {
+            if (context.user){
+                const updatedUser = await User.findByIdAndUpdate(
+                    context.user._id,
+                    {$pull: {savedBooks: {bookId}}},
+                    {new: true}
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
         }
-    },
+    }, 
 }
+
+module.exports = resolvers;
