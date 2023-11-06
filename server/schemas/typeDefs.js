de752f63e7e5fdcedfd4e30d45f7ce9f0decc843
Bryan Bickel <briimcfly@gmail.com>
2023-11-06 12:01:40 -0800

    ggl vs gql typo in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
-const {ggl} = require('apollo-server-express');
+const {gql} = require('apollo-server-express');
 
-const typeDefs = ggl`
+const typeDefs = gql`
 type User{
     _id: ID
     username: String
